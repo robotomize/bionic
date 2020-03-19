9a9badd3520dc46f1fe429f4ed595e63ddd94e44
robotomize <robotomize@gmail.com>
2020-03-20 00:54:59 +1100

    Added new configuration type, basic clients

--- bionic.go
+++ bionic.go
@@ -2,50 +2,86 @@ package bionic
 
 import (
 	"context"
-	"github.com/labstack/gommon/log"
+	"fmt"
+	"github.com/google/uuid"
+	"sync"
+	"sync/atomic"
 )
 
 const (
 	DefaultRunnerNumber = 1
+	DefaultAddr         = "localhost:53300"
 )
+
+var defaultOptions = Options{
+	runnerNumber: DefaultRunnerNumber,
+	addr:         DefaultAddr,
+}
 
 type BHook func() error
 
 type Manager struct {
 	hooks  []BHook
 	taskCh chan interface{}
-	cfg    *ManagerConfig
+	opts   Options
 	cancel func()
+
+	cMu     sync.RWMutex
+	clients map[uuid.UUID]*Client
 }
 
-type ManagerConfig struct {
-	runnerNumber int
-}
-
-func New() *Manager {
+func New(opt ...Option) *Manager {
+	opts := defaultOptions
+	for _, o := range opt {
+		o.apply(&opts)
+	}
 	return &Manager{
-		cfg: NewConfig(DefaultRunnerNumber),
+		hooks:   []BHook{},
+		taskCh:  make(chan interface{}, 1),
+		opts:    opts,
+		cancel:  nil,
+		cMu:     sync.RWMutex{},
+		clients: map[uuid.UUID]*Client{},
 	}
 }
 
-func NewConfig(runnerNumber int) *ManagerConfig {
-	return &ManagerConfig{
-		runnerNumber: runnerNumber,
-	}
+type Option interface {
+	apply(*Options)
 }
 
-func (m *Manager) AddCustomConfig(cfg *ManagerConfig) {
-	m.cfg = cfg
+type Options struct {
+	runnerNumber int
+	addr         string
+}
+
+type funcOptions struct {
+	f func(*Options)
+}
+
+func (o *funcOptions) apply(opts *Options) {
+	o.f(opts)
+}
+
+func newFuncOptions(f func(*Options)) *funcOptions {
+	return &funcOptions{f: f}
+}
+
+func SetRunnersNumber(n int) Option {
+	return newFuncOptions(func(o *Options) {
+		o.runnerNumber = n
+	})
+}
+
+func SetAddr(addr string) Option {
+	return newFuncOptions(func(o *Options) {
+		o.addr = addr
+	})
 }
 
 func (m *Manager) Run() {
-	if m.cfg == nil {
-		m.cfg = NewConfig(DefaultRunnerNumber)
-	}
-	m.taskCh = make(chan interface{}, 1)
 	runnerCtx, cancel := context.WithCancel(context.Background())
 	m.cancel = cancel
-	for i := 0; i < m.cfg.runnerNumber; i++ {
+	for i := 0; i < m.opts.runnerNumber; i++ {
 		m.runner(runnerCtx)
 	}
 }
@@ -82,7 +118,46 @@ func (m *Manager) RegisterHook(h ...BHook) {
 func (m *Manager) executeHooks() {
 	for _, h := range m.hooks {
 		if err := h(); err != nil {
-			log.Error(err)
+			fmt.Printf(err.Error())
 		}
 	}
+}
+
+type Task struct {
+}
+
+const (
+	ClientWorking uint32 = iota
+	ClientWaiting
+	ClientDisconnected
+)
+
+type Client struct {
+	ID       uuid.UUID
+	State    uint32
+	Hostname string
+}
+
+func newClient() *Client {
+	return &Client{
+		ID:       uuid.New(),
+		State:    ClientWaiting,
+		Hostname: "",
+	}
+}
+
+func (c *Client) getState() uint32 {
+	return atomic.LoadUint32(&c.State)
+}
+
+func (c *Client) toWorking() {
+	atomic.StoreUint32(&c.State, ClientWorking)
+}
+
+func (c *Client) toWaiting() {
+	atomic.StoreUint32(&c.State, ClientWaiting)
+}
+
+func (c *Client) toDisconnected() {
+	atomic.StoreUint32(&c.State, ClientDisconnected)
 }
--- examples/main.go
+++ examples/main.go
@@ -7,4 +7,5 @@ import (
 func main() {
 	m := &bionic.Manager{}
 	_ = m
+
 }
